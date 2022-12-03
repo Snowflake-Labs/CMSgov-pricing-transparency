f087befac9ec3db20f4290b0926fec7360d3154b
sfc-gh-vsekar <venkatesh.sekar@snowflake.com>
2022-12-03 15:48:45 -0500

    safety checkin

--- src/python/in_network_udtf/negotiation_arrangements.py
+++ src/python/in_network_udtf/negotiation_arrangements.py
@@ -9,6 +9,7 @@ import _snowflake
 import shutil
 import simplejson as sjson
 import hashlib
+import datetime
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logger = logging.getLogger("negotiation_arragnments")
@@ -55,10 +56,9 @@ def parse_breakdown_save(p_session: Session ,p_approx_batch_size: int
     datafl_basename = get_basename_of_datafile(p_datafile)
     out_folder = os.path.join('/tmp', datafl_basename)
    
-    rdata = ''
     files_written = -1
     seq_no = -1
-    for idx ,rec in enumerate(ijson.items(f, 'in_network.item' ,use_float=True)):
+    for rec in ijson.items(f, 'in_network.item' ,use_float=True):
         seq_no += 1
 
         if seq_no < p_from_seg:
@@ -66,8 +66,6 @@ def parse_breakdown_save(p_session: Session ,p_approx_batch_size: int
         elif (seq_no > p_to_seg):
             break
 
-        # segment_id = str(uuid.uuid4())
-        #innetwork_hdr = {x: rec[x] for x in rec if x not in ['negotiated_rates' ,'bundled_codes' ,'covered_services']}
         innetwork_hdr = rec.copy()
         entries_to_remove = ['negotiated_rates' ,'bundled_codes' ,'covered_services']
         for k in entries_to_remove:
@@ -76,7 +74,7 @@ def parse_breakdown_save(p_session: Session ,p_approx_batch_size: int
         negotiation_arrangement = str(innetwork_hdr['negotiation_arrangement'])
         seg_name = str(innetwork_hdr['name'])
         header_id = f'''{negotiation_arrangement}::{seg_name}'''
-        segment_id = hashlib.md5(header_id.encode())
+        segment_id = hashlib.md5(header_id.encode()).hexdigest()
 
         innetwork_hdr['SEQ_NO'] = seq_no
         innetwork_hdr['DATA_FILE'] = p_datafile
@@ -100,6 +98,9 @@ def parse_breakdown_save(p_session: Session ,p_approx_batch_size: int
             out_file = os.path.join(out_folder ,f'{segment_id}' , f'data_{seq_no}_{chunk_idx}.parquet.gz')
             #automatically create parent folders if it does not exists to avoid errors
             os.makedirs(os.path.dirname(out_file), exist_ok=True)
+            
+            # TODO delete if not better approach
+            # store file as json
             # with open(out_file, "w") as out_fl:
             #     rec_str = sjson.dumps(curr_rec)
             #     out_fl.write(rec_str)
@@ -108,6 +109,7 @@ def parse_breakdown_save(p_session: Session ,p_approx_batch_size: int
             #     rec_str = sjson.dumps(curr_rec)
             #     out_fl.write(rec_str)
         
+            # store as parquet for better read performance than JSON
             df = pd.json_normalize(curr_rec)
             df.to_parquet(out_file,compression='gzip')  
             files_written += 1
@@ -115,8 +117,8 @@ def parse_breakdown_save(p_session: Session ,p_approx_batch_size: int
             #if files_written >= l_approx_batch_size:
         upload_segments_file_to_stage(p_session ,out_folder ,p_target_stage ,datafl_basename)
         shutil.rmtree(out_folder)
-        files_written = -1
 
+    #upload any residual
     upload_segments_file_to_stage(p_session ,out_folder ,p_target_stage ,datafl_basename)
     # shutil.rmtree(out_folder)
     # p_session.sql(f'alter stage {p_target_stage} refresh; ').collect()
@@ -154,14 +156,30 @@ def parse_breakdown_save_wrapper(p_session: Session ,p_approx_batch_size: int
 
 def main(p_session: Session ,p_approx_batch_size: int 
     ,p_stage_path: str ,p_datafile: str ,p_target_stage: str
-    ,p_from_seg: int ,p_to_seg: int):
+    ,p_from_seg: int ,p_to_seg: int ,p_task_name: str):
+    
     ret = {}
     ret['data_file'] = p_datafile
+    ret['start_rec_num'] = p_from_seg
+    ret['end_rec_num'] = p_to_seg
+
+    start = datetime.datetime.now()
     
     seg_record_counts = parse_breakdown_save_wrapper(p_session ,p_approx_batch_size 
         ,p_stage_path ,p_datafile ,p_target_stage
         ,p_from_seg ,p_to_seg)
+    end = datetime.datetime.now()
 
+    elapsed = (end - start)
+    ret['elapsed'] =  f'=> {elapsed} '
     ret['ingested_record_counts'] = seg_record_counts
+    
+    ret_str = str(ret)
+    ret_str = ret_str.replace('\'', '"')
+    sql_stmt = f'''
+        insert into segment_task_execution_status(task_name ,task_ret_status) values('{p_task_name}' ,'{ret_str}');
+    '''
+    p_session.sql(sql_stmt).collect()
+    
     ret['status'] = True
     return ret
