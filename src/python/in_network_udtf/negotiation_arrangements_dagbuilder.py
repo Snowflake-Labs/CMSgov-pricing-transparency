d3097f96d24f5a61f54fbdb6ad1608d89e74ffff
sfc-gh-vsekar <venkatesh.sekar@snowflake.com>
2022-12-04 08:29:25 -0500

    commit safety

--- src/python/in_network_udtf/negotiation_arrangements_dagbuilder.py
+++ src/python/in_network_udtf/negotiation_arrangements_dagbuilder.py
@@ -6,10 +6,17 @@ import snowflake.snowpark.functions as F
 import hashlib
 from typing import List
 
-logger = logging.getLogger("negotiation_arrangements_dagbuilder")
-BATCH_SIZE = 5000
-IN_NETWORK_RATES_SEGHDR_TBL = 'in_network_rates_segment_header'
 TASK_TO_SEGMENTIDS_TBL = 'task_to_segmentids'
+ASSUMED_TOTAL_NR_SEGMENTS = 1000 * 20
+
+logging.basicConfig(stream=sys.stdout, level=logging.ERROR)
+logger = logging.getLogger("negotiation_arrangements_dagbuilder")
+
+def get_basename_of_datafile(p_datafile:str) -> str:
+    base = os.path.basename(p_datafile)
+    base = base.replace('-','_')
+    fl_base = os.path.splitext(base)
+    return fl_base[0]
 
 def get_snowpark_dataframe(p_session: Session ,p_df: pd.DataFrame):
     # Convert the data frame into Snowpark dataframe, needed for merge operation
@@ -26,8 +33,6 @@ def append_to_table(p_session: Session ,p_df: pd.DataFrame ,p_target_tbl: str):
     #ref: https://docs.snowflake.com/ko/developer-guide/snowpark/reference/python/_autosummary/snowflake.snowpark.html#snowflake.snowpark.Session.write_pandas
 
     logger.info(f'Appending batch to table [{p_target_tbl}] ...')
-    # tbl_spdf = p_session.write_pandas(p_df ,table_name=p_target_tbl 
-    #     ,quote_identifiers=False ,auto_create_table=True ,overwrite = False ,table_type='transient')
     
     # Convert the data frame into Snowpark dataframe, needed for merge operation
     sp_df = get_snowpark_dataframe(p_session ,p_df)
@@ -56,88 +61,43 @@ def split_range_into_buckets(p_range_max, p_num_buckets):
         round(step*i)+1 if i >= 1 else 0
         ,round(step*(i+1))) for i in range(p_num_buckets)]
 
-def segments_count_balance(p_session: Session ,p_datafile: str ,p_parallels: int):
-    logger.info(f'Mapping tasks to segments parallel: {p_parallels} datafile {p_datafile}')
+def reshape_tasks_to_matrix(p_tasks_count: int ,p_parallel: int):
+    t = p_tasks_count
+    m = -1
+    n = -1
+    # Keep the matrix in 20 X 10 matrix
+    for x in range(21):
+        for y in range(11):
+            if x*y == t:
+                m = max(x ,y)
+                n = min(x, y)
+                break
+        if m != -1:
+            break
 
-    # Get the negotiated arrangements segments to negotiated_rates_count
-    sql_stmt = f'''
-        select ROW_NUMBER() OVER( order by segment_id asc) as seq_no
-            ,segment_id, negotiated_rates_count
-        from in_network_rates_segment_header_V2
-        where data_file = '{p_datafile}'
-        order by seq_no
-    '''
-    i_df = p_session.sql(sql_stmt).to_pandas()
+    #choosen_shape = f'{m} X {n} => {t}'
+    task_matrix_shape = (m,n)
+    return task_matrix_shape
 
-    max_rec_num = len(i_df)
-    ranges = split_range_into_buckets(max_rec_num ,p_parallels)
+def create_root_task_and_fh_loader(p_session: Session  
+    ,p_stage_path: str ,p_datafile: str ,p_warehouse: str): 
+    logger.info(f'Creating the root task ddl ...')
 
-    o_df_rows = []
-    for idx,(start_idx ,end_idx) in enumerate(ranges):
-        sub_df = i_df.iloc[start_idx:end_idx,]
-        segment_ids = []
-        nr_c = -1
-        for index, row in sub_df.iterrows():
-            segment_ids.append(row['SEGMENT_ID'])
-            nr_c += row['NEGOTIATED_RATES_COUNT']
-
-        o_df_rows.append( (','.join(segment_ids) ,nr_c) )
-        o_df = pd.DataFrame(o_df_rows
-        ,columns=['SEGMENT_IDS', 'NEGOTIATED_RATES_COUNT'])
-
-    return o_df
-
-def save_tasks_to_segments(p_session: Session ,p_datafile: str ,p_df :pd.DataFrame ,p_force_rerun: bool):
-    logger.info('Saving tasks to segment')
-
-    rows = []
-    m = get_md5of_datafile(p_datafile)
-    for idx, row in p_df.iterrows():
-        task_name = f'tsk_{m}_{idx}'
-        seg_ids = row['SEGMENT_IDS']
-        nr_c = row['NEGOTIATED_RATES_COUNT']
-
-        # Remove DUMMY segment id
-        t = [s for s in seg_ids.split(',') if s != 'DUMMY']
-        segment_count = len(t)
-        segment_ids = ','.join(t)
-
-        r = (idx ,p_datafile ,task_name
-                ,segment_ids ,segment_count ,nr_c)
-        rows.append(r)
-    
-    t_df = pd.DataFrame(rows
-        ,columns=['BUCKET' ,'DATA_FILE' ,'ASSIGNED_TASK_NAME' ,'SEGMENT_IDS'
-            ,'SEGMENTS_COUNT', 'SEGMENTS_RECORD_COUNT'])
-        
-    if p_force_rerun == True:
-        tbl_df = p_session.table(TASK_TO_SEGMENTIDS_TBL)
-        tbl_df.delete(tbl_df["DATA_FILE"] == F.lit(f'{p_datafile}'))
-
-    append_to_table(p_session ,t_df ,TASK_TO_SEGMENTIDS_TBL)
-
-    return t_df
-
-def get_md5of_datafile(p_datafile: str):
-    return hashlib.md5(p_datafile.encode()).hexdigest()
-
-def create_root_task_and_fh_loader(p_session: Session ,p_root_task_name: str ,p_stage_path: str ,p_datafile: str ,p_warehouse: str): 
-    logger.info(f'Creating the root task {p_root_task_name} ddl ...')
-
-    m = get_md5of_datafile(p_datafile)
-    fh_task_name = f'tsk_fh_{m}'
+    fl_basename = get_basename_of_datafile(p_datafile)
+    root_task_name = f'''DAG_ROOT_{fl_basename}'''
+    fh_task_name = f'tsk_fh_{fl_basename}'
 
     sql_stmts = [
-        f'alter task if exists {p_root_task_name} suspend;' 
+        f'alter task if exists {root_task_name} suspend;' 
         ,f'''
-            create or replace task {p_root_task_name}
+            create or replace task {root_task_name}
                 warehouse = {p_warehouse}
                 schedule = 'using cron 30 2 L 6 * UTC'
                 comment = 'DAG to load data for file: {p_datafile}'
                 as
                 begin
                     insert into segment_task_execution_status( data_file  ,task_name) 
-                        values('{p_datafile}' ,'{p_root_task_name}');
+                        values('{p_datafile}' ,'{root_task_name}');
                 end;
         '''
         # ,f'''
@@ -153,10 +113,42 @@ def create_root_task_and_fh_loader(p_session: Session ,p_root_task_name: str ,p_
     for stmt in sql_stmts:
         p_session.sql(stmt).collect()
 
-    return p_root_task_name ,fh_task_name
+    return root_task_name ,fh_task_name
+
+def save_tasks_to_segments(p_session: Session ,p_datafile: str ,p_parallel: int ,p_force_rerun: bool):
+    logger.info('Saving tasks to segment')
+
+    rows = []
+    fl_basename = get_basename_of_datafile(p_datafile)
+
+    splits = split_range_into_buckets(ASSUMED_TOTAL_NR_SEGMENTS,p_parallel)
+    logger.info(f'task splits : {len(splits)} ')
+    #task_matrix_shape = reshape_buckets_to_matrix(splits ,parallel_rows)
+    #print(task_matrix_shape)
+
+    for idx,(m ,n) in enumerate(splits):
+        task_name = f'''T_{fl_basename}_{m}_{n}'''
+        splits_info = f'{m} X {n}'
+        segment_count = n - m
+        
+        r = (idx ,p_datafile ,task_name
+                ,splits_info ,segment_count ,-1)
+        rows.append(r)
+    
+    t_df = pd.DataFrame(rows
+        ,columns=['BUCKET' ,'DATA_FILE' ,'ASSIGNED_TASK_NAME' ,'SEGMENT_IDS'
+            ,'SEGMENTS_COUNT', 'SEGMENTS_RECORD_COUNT'])
+        
+    if p_force_rerun == True:
+        tbl_df = p_session.table(TASK_TO_SEGMENTIDS_TBL)
+        tbl_df.delete(tbl_df["DATA_FILE"] == F.lit(f'{p_datafile}'))
+
+    append_to_table(p_session ,t_df ,TASK_TO_SEGMENTIDS_TBL)
+
+    return t_df
 
 def create_subtasks(p_session: Session ,p_root_task_name: str 
-    ,p_approx_batch_size: int ,p_stage_path: str  ,p_datafile: str
+    ,p_approx_batch_size: int ,p_stage_path: str  ,p_datafile: str ,p_target_stage: str
     ,p_warehouse: str ,p_task_lists:List[str] ,task_matrix_shape):
     logger.info(f'Creating the task ddl ...')
     line_end_tasks = []
@@ -169,6 +161,11 @@ def create_subtasks(p_session: Session ,p_root_task_name: str
         for n in range(task_matrix_shape[1]):
             task_name = p_task_lists[idx]
             
+            #taskname format: task_name = f'''T_{fl_basename}_{m}_{n}'''
+            l = len(task_name.split('_'))
+            range_to = task_name.split('_')[l-1]
+            range_from = task_name.split('_')[l-2]
+
             # 86400000 => 1 day
             # 3600000 => 1 hour
             sql_stmts = [
@@ -176,14 +173,15 @@ def create_subtasks(p_session: Session ,p_root_task_name: str
                     create or replace task {task_name}
                     warehouse = {p_warehouse}
                     user_task_timeout_ms = 86400000
+                    comment = 'negotiated_arrangements segment range [{range_from} - {range_to}] data ingestor '
                     after {preceding_task}
                     as
                     begin
-                        call innetwork_rates_segments_ingest_sp(
-                            {p_approx_batch_size} ,'{p_stage_path}' ,'{p_datafile}'
-                            ,'negotiated_rates'
-                            ,'{task_name}');
-
+                        call innetwork_rates_segheader(
+                            {p_approx_batch_size} 
+                            ,'{p_stage_path}' ,'{p_datafile}' ,'{p_target_stage}' 
+                            ,{range_from} ,{range_to} ,'{task_name}');
+                        
                         alter task if exists {task_name} suspend;
                     end;
                 '''
@@ -199,36 +197,13 @@ def create_subtasks(p_session: Session ,p_root_task_name: str
         if end_task_name != '':
             line_end_tasks.append(end_task_name)
     
-    # for task_name in p_task_lists:
-    #     sql_stmts = [
-    #         f'''
-    #             create or replace task {task_name}
-    #             warehouse = {p_warehouse}
-    #             schedule = 'using cron 30 2 L 6 * UTC'
-    #             as
-    #             begin
-    #                 call innetwork_rates_segments_ingest_sp(
-    #                     {p_approx_batch_size} ,'{p_stage_path}' ,'{p_datafile}'
-    #                     ,'negotiated_rates'
-    #                     ,'{task_name}');
-
-    #                 alter task if exists {task_name} suspend;
-    #             end;
-    #         '''
-    #         ,f''' alter task if exists  {task_name} resume; '''
-    #         ,f''' execute task {task_name}; '''
-    #     ]
-    #     for stmt in sql_stmts:
-    #         p_session.sql(stmt).collect()
-    #     line_end_tasks.append(task_name)
-
     return line_end_tasks
 
 def create_term_tasks(p_session: Session ,p_datafile: str
     ,p_warehouse: str ,p_root_task_name: str  ,p_line_end_task_lists:List[str] ,p_task_lists:List[str]):
     logger.info(f'Creating the task ddl ...')
 
-    m = get_md5of_datafile(p_datafile)
+    m = get_basename_of_datafile(p_datafile)
     term_task_name = f'TERM_tsk_{m}'
     
     after_tasks_phrase = ','.join(p_line_end_task_lists)
@@ -260,43 +235,29 @@ def create_term_tasks(p_session: Session ,p_datafile: str
 
     return term_task_name
 
-def reshape_tasks_to_matrix(p_tasks_count: int ,p_parallels: int):
-    t = p_tasks_count
-    m = -1
-    n = -1
-    for x in range(20):
-        for y in range(20):
-            if x*y == t:
-                m = max(x ,y)
-                n = min(x, y)
-                break
-        if m != -1:
-            break
-
-    #choosen_shape = f'{m} X {n} => {t}'
-    task_matrix_shape = (m,n)
-    return task_matrix_shape
-
-def main(p_session: Session ,p_approx_batch_size: int ,p_stage_path: str  ,p_datafile: str ,p_parallels: int ,p_warehouse: str ,p_force_rerun: bool):
+def main(p_session: Session ,p_approx_batch_size: int 
+    ,p_stage_path: str ,p_datafile: str ,p_target_stage: str
+    ,p_parallel: int ,p_warehouse: str 
+    ,p_force_rerun: bool):
     ret = {}
     ret['data_file'] = p_datafile
 
     # map segments to tasks. the task will parse and ingest the specified segments
-    seg_df = segments_count_balance(p_session ,p_datafile ,p_parallels)
-    task_to_segments_df = save_tasks_to_segments(p_session ,p_datafile ,seg_df ,p_force_rerun)
-
-    root_task_name = get_md5of_datafile(p_datafile)
-    root_task_name = f'''DAG_ROOT_{root_task_name}'''
+    task_to_segments_df = save_tasks_to_segments(p_session ,p_datafile ,p_parallel ,p_force_rerun)
 
     # create root task
-    rt_task, fh_task_name = create_root_task_and_fh_loader(p_session ,root_task_name ,p_stage_path ,p_datafile ,p_warehouse)
+    root_task_name, fh_task_name = create_root_task_and_fh_loader(p_session  
+        ,p_stage_path ,p_datafile ,p_warehouse)
+    ret['root_task'] = root_task_name
 
     # create sub tasks and add to root task
     task_list = list(task_to_segments_df.ASSIGNED_TASK_NAME.values)
-    task_matrix_shape = reshape_tasks_to_matrix( len(task_list) ,p_parallels)
+    task_matrix_shape = reshape_tasks_to_matrix( len(task_list) ,p_parallel)
     ret['task_matrix_shape'] = task_matrix_shape
     
-    line_end_tasks = create_subtasks(p_session ,root_task_name ,p_approx_batch_size ,p_stage_path ,p_datafile ,p_warehouse ,task_list ,task_matrix_shape)
+    line_end_tasks = create_subtasks(p_session ,root_task_name ,p_approx_batch_size 
+        ,p_stage_path ,p_datafile ,p_target_stage
+        ,p_warehouse ,task_list ,task_matrix_shape)
     # line_end_tasks.append(fh_task_name)
 
     # create term task
@@ -304,3 +265,6 @@ def main(p_session: Session ,p_approx_batch_size: int ,p_stage_path: str  ,p_dat
     
     ret['status'] = True
     return ret
+
+
+#-----------
