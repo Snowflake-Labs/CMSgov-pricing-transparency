f63233806d9967a5284e5a9d0d65060cc905f96e
sfc-gh-vsekar <venkatesh.sekar@snowflake.com>
2023-03-20 21:23:23 -0400

    code reorg

--- src/streamlit/pages/99_Ingest_Sample_data_v0.py
+++ src/streamlit/pages/99_Ingest_Sample_data_v0.py
@@ -1,7 +1,7 @@
 from snowflake.snowpark.session import Session
 import streamlit as st
 import logging ,sys
-from util_fns import exec_sql_script ,exec_python_script ,exec_shell_script
+import util_fns as U
 
 # Import the commonly defined utility scripts using
 # dynamic path include
@@ -9,126 +9,23 @@ import sys
 sys.path.append('src/python/lutils')
 import sflk_base as L
 
-sys.path.append('src/streamlit')
-import error_page as error_page
-
 # Define the project home directory, this is used for locating the config.ini file
 PROJECT_HOME_DIR='.'
 
 logging.basicConfig(stream=sys.stdout, level=logging.ERROR)
-logger = logging.getLogger('exec_sql_script')
+logger = logging.getLogger('3_Ingest_sampledata')
 
+# Initialize a session with Snowflake
 config = L.get_config(PROJECT_HOME_DIR)
 sp_session = None
 if "snowpark_session" not in st.session_state:
     sp_session = L.connect_to_snowflake(PROJECT_HOME_DIR)
-    sp_session.use_role(f'''{config['SNOW_CONN']['role']}''')
-    sp_session.use_schema(f'''{config['SNOW_CONN']['database']}.{config['SNOW_CONN']['schema']}''')
+    sp_session.use_role(f'''{config['APP_DB']['role']}''')
+    sp_session.use_schema(f'''{config['APP_DB']['database']}.{config['APP_DB']['schema']}''')
     sp_session.use_warehouse(f'''{config['SNOW_CONN']['warehouse']}''')
     st.session_state['snowpark_session'] = sp_session
 else:
     sp_session = st.session_state['snowpark_session']
-
-#-----------------------------------------------------
-# Run the Setup scripts
-
-def page_header():
-    # Header.
-    st.image("src/streamlit/images/logo-sno-blue.png", width=100)
-    st.subheader("Pricing Transperancy")
-
-def custom_page_styles():
-    # page custom Styles
-    st.markdown("""
-                <style>
-                    #MainMenu {visibility: hidden;} 
-                    header {height:0px !important;}
-                    footer {visibility: hidden;}
-                    .block-container {
-                        padding: 0px;
-                        padding-top: 15px;
-                        padding-left: 1rem;
-                        max-width: 98%;
-                    }
-                    [data-testid="stVerticalBlock"] {
-                        gap: 0;
-                    }
-                    [data-testid="stHorizontalBlock"] {
-                        width: 99%;
-                    }
-                    .stApp {
-                        width: 98% !important;
-                    }
-                    [data-testid="stMetricLabel"] {
-                        font-size: 1.25rem;
-                        font-weight: 700;
-                    }
-                    [data-testid="stMetricValue"] {
-                        font-size: 1.5rem;
-                        color: gray;
-                    }
-                    [data-testid="stCaptionContainer"] {
-                        font-size: 1.25rem;
-                    }
-                    [data-testid="stMarkdownContainer"] {
-                        font-size: 2rem;
-                    }
-                    [data-testid="metric-container"] {
-                        word-wrap: break-word;
-                    }
-                    div.stButton > button:first-child {
-                        background-color: #50C878;color:white; border-color: none;
-                </style>""", unsafe_allow_html=True)
-
-def display_connection_info():
-    with st.expander("Snowflake Connection Information", expanded=True):
-        c1, s1, c2, s2, c3, s3, c4, s4, c5 = st.columns([1, 0.1, 2.5, 0.1, 2.5, 0.1, 1, 0.1, 1])
-        with s1:
-            st.markdown(
-                """<div style="border: 1px solid lightgrey; height: 60px; width:2px; background-color: lightgrey;"></div> """,
-                unsafe_allow_html=True)
-
-        with s2:
-            st.markdown(
-                """<div style="border: 1px solid lightgrey; height: 60px; width:2px; background-color: lightgrey;"></div> """,
-                unsafe_allow_html=True)
-
-        with s3:
-            st.markdown(
-                """<div style="border: 1px solid lightgrey; height: 60px; width:2px; background-color: lightgrey;"></div> """,
-                unsafe_allow_html=True)
-        with s4:
-            st.markdown(
-                """<div style="border: 1px solid lightgrey; height: 60px; width:2px; background-color: lightgrey;"></div> """,
-                unsafe_allow_html=True)
-
-        with c1:
-            account = sp_session.get_current_account().replace('"', '')
-            st.metric("Account", f"{account}")
-
-        with c2:
-            st.metric("IOTBridge staging database", f"{config['APP_DB']['database']}.{config['APP_DB']['schema']}")
-
-        with c3:
-            st.metric("Modelling database", f"{config['APP_DB']['database']}.{config['APP_DB']['schema']}")
-
-        with c4:
-            st.metric("Warehouse", f"{config['SNOW_CONN']['warehouse']}")
-
-        with c5:
-            st.metric("Role", f"{config['SNOW_CONN']['role']}")
-
-    st.markdown("""<hr style="height:40px; font-size:2px; width:99%; border:none;color:none;" /> """,
-                unsafe_allow_html=True)
-
-def build_UI():
-    custom_page_styles()
-    page_header()
-    st.markdown(
-        """<hr style="height:2px; width:100%; border:none;color:lightgrey;background-color:lightgrey;margin-bottom: 16px; margin-top: 25px;" /> """,
-        unsafe_allow_html=True)
-
-    display_connection_info()
 
 #-----------------------------------------------------
 import pandas as pd
